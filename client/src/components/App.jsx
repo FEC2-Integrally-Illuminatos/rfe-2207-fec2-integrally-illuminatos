febd8728b3f8fec586ad6bb0ce921add16a584b1
Karrissa Volcy <v.karrissa1@gmail.com>
2022-08-26 10:24:20 -0400

    webpack changes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import Question from './faq/Question.jsx';
 
 const App = () => {
-  return <h1>Hello World</h1>
+  return (
+    <>
+    <h1>Hello World</h1>
+    <Question/>
+    </>
+  )
 }
 
 export default App;
---
+++ client/src/components/faq/Question.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Question = (props) => {
+  return (
+    <div>Q: This is a question?</div>
+  )
+}
+
+export default Question;
