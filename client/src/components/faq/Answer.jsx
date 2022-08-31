1c4dcc97d665bb869f7fae6cd31a5fe51b3fc5a2
Karrissa Volcy <v.karrissa1@gmail.com>
2022-08-31 08:53:01 -0400

    photos now rendering with answers

--- client/src/components/faq/Answer.jsx
+++ client/src/components/faq/Answer.jsx
@@ -4,9 +4,13 @@ import Helpful from './Helpful.jsx';
 import Report from './Report.jsx';
 
 export default function Answer({answer}) {
+  console.log(answer);
   return (
     <div>
       {answer.body}<br></br>
+      {answer.photos.length > 0 && <div>{answer.photos.map((photo) => {
+        return (<img src={photo.url} alt="uploaded photo" key={photo.id}></img>)
+      })}</div>}
     <User name={answer['answerer_name']} date={answer.date}/>
     <Helpful count={answer.helpfulness} answerId={answer['answer_id']}/> | <Report answerId={answer['answer_id']}/>
     <br></br>
