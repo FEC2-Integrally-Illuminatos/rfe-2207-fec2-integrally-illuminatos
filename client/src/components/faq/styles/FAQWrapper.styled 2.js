ddb9f1439f178a55acbc07d0c0274c948b9b7018
Karrissa Volcy <v.karrissa1@gmail.com>
2022-09-01 10:50:04 -0400

    merging main to faq2

---
+++ client/src/components/faq/styles/FAQWrapper.styled 2.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const FAQWrapper = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  gid-template-rows: 0.25fr 0.75fr;
+  grid-template-areas:
+    "search search search search"
+    "main main main buttons";
+  gap: 15px 10px;
+`
+
+//TODO: Add media queries
