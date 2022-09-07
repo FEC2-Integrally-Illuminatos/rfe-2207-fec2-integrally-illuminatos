81100d13156e48c40d8bba793a3e83297503b01b
Karrissa Volcy <v.karrissa1@gmail.com>
2022-09-07 13:47:57 -0400

    image popup resize

--- client/src/components/faq/Answer.jsx
+++ client/src/components/faq/Answer.jsx
@@ -24,20 +24,54 @@ const Body = styled.div`
 const Img = styled.img`
   margin: auto;
   display: block;
+  border-radius: 8px;
+  width: 60%;
+  height: auto;
+  max-height: 70%;
+`
+const Content = styled.div`
+  background-color: #D8CFD0;
+  margin: auto;
+  padding: 20px;
+  border: 1px solid black;
+  border-radius: 15px;
+  width: 80%;
+  height: auto;
+  max-height: 90%;
 `
 
+const CloseBtn = styled.button`
+  color:black;
+  font-size: 35px;
+  padding: 20px;
+  outline: none;
+  background-color: transparent;
+  background-repeat: no-repeat;
+  border: none;
+  cursor: pointer;
+  &:hover {
+    color: rgb(255, 255, 255);
+  }
+`
 export default function Answer({answer, fetchAnswers}) {
   const [openImg, setOpenImg] = useState(false);
-  const popUpImg = () => {
+  const [imgSrc, setImgSrc] = useState('');
+  const popUpImg = (src) => {
     setOpenImg(!openImg);
+    setImgSrc(src);
   }
   return (
     <Wrapper>
       {answer.body}<br></br>
       {answer.photos.length > 0 && <Div>{answer.photos.map((photo) => {
         return (
-          <Image onClick={popUpImg} src={photo.url} alt="uploaded photo" key={photo.id} width="80" height="auto"></Image>
-
+          <>
+            <Image onClick={() => {popUpImg(photo.url)}} src={photo.url} alt="uploaded photo" key={photo.id} width="80" height="auto" key={photo.url}></Image>
+            {openImg && <ModalWrap><Content>
+              <CloseBtn onClick={() => {setOpenImg(false)}}>X</CloseBtn>
+              <Img src={imgSrc}></Img>
+              </Content></ModalWrap> }
+          </>
           )
         })}</Div>}
       <Body>
@@ -49,4 +83,3 @@ export default function Answer({answer, fetchAnswers}) {
     </Wrapper>
   )
 }
-// {openImg && <ModalWrap><Img src={photo.url}></Img></ModalWrap> }
