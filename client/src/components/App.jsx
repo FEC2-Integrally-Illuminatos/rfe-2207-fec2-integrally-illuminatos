a84893b17def212220899735c5e33dea629b0a3d
August Voelker <gusvoelker@outlook.com>
2022-09-08 10:08:17 -0400

    change loading screen to last until products load

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -40,17 +40,13 @@ const App = () => {
         setStyles(data.data.styles);
         console.log('this is the data', data.data);
       })
+      .then(() => {
+        setLoading(false);
+      })
       .catch((err) => {
         console.log(err);
       });
   }, [currentProductID])
-
-  useEffect(() => {
-    axios.get('/loading')
-      .then(() => {
-        setLoading(false);
-      });
-  });
 
   const handleProductChange = (e) => {
     let productNum = null;
@@ -98,7 +94,7 @@ const App = () => {
       <Router>
         <NavBar/>
           <Chevere>
-            {/* <Overview product={product} style={style} styles={styles}/> */}
+            <Overview product={product} style={style} styles={styles}/>
             <RelatedProducts currentProductID={currentProductID} handleProductChange={handleProductChange} setProduct={setProduct} product={product} userOutfits={userOutfits} setUserOutfits={setUserOutfits} handleAddClick={handleAddClick}/>
             <Wrapper product={product}/>
             <Reviews product={product}/>
